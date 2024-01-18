888056d3a01c518baf629cb607ff56df9fd4d4b8
Vllaadd <vlladdiimiir@gmail.com>
2024-01-18 05:09:51 -0500

    server created

---
+++ server/server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+
+// MIDDLEWARE
+const app = express();
+const.use(express.json());
+
+app.listen(3000, () => console.log('Server started on port 3000'));
